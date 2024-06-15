9d188335f3c249a956fd8b6ae1201f3cb34dfcc6
weaver010 <abdelrahmannagaty839@gmail.com>
2024-06-15 19:58:25 +0000

    hi

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- SQL script that creates a trigger that resets 
+DELIMITER $$
+CREATE TRIGGER reset 
+BEFORE UPDATE
+ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+
+DELIMITER;
