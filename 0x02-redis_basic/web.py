6153e146541bf903756b45c1794ea7a6528c010a
weaver010 <abdelrahmannagaty839@gmail.com>
2024-06-20 18:49:17 +0000

    hi

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,29 +9,28 @@ from typing import Callable
 
 
 def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for caching and tracking get_page function calls """
+    """ Decorator for get_page
+    """
     @wraps(fn)
     def wrapper(url: str) -> str:
-        """ Wrapper function that:
-            - Increments the call count for the given URL
-            - Checks if the URL's response is cached in Redis
-            - Retrieves cached data if available, otherwise makes an HTTP request
-            - Caches the response in Redis with a timeout of 10 seconds
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
         """
         client = redis.Redis()
-        client.incr(f'count:{url}')  # Increment call count for this URL
+        client.incr(f'count:{url}')
         cached_page = client.get(f'{url}')
         if cached_page:
-            return cached_page.decode('utf-8')  # Return cached response if available
-        response = fn(url)  # Call original function to fetch data
-        client.set(f'{url}', response, 10)  # Cache response in Redis for 10 seconds
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
         return response
     return wrapper
 
 
 @track_get_page
 def get_page(url: str) -> str:
-    """ Makes an HTTP GET request to the specified URL and returns the response text """
+    """ Makes a http request to a given endpoint
+    """
     response = requests.get(url)
     return response.text
-
