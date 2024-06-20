5b2e375e78001e7b482f65786ddf1d6cb9903593
weaver010 <abdelrahmannagaty839@gmail.com>
2024-06-20 18:55:08 +0000

    hi

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
 """
-sample use case of caching
+Caching request module
 """
-
-from requests import get
 import redis
+import requests
 from functools import wraps
+from typing import Callable
 
 
-def count_decorator(func: callable) -> callable:
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
     """
-    decorator to enable caching
-    """
-    @wraps
-    def cache(url):
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
         """
-        wrapper function
-        """
-        cache = redis.Redis()
-        key = "count:{}".format(url)
-        if not cache.exists(key):
-            cache.setex(key, 10, 1)
-        else:
-            cache.incr(key)
-        return func(url)
-    return cache
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
 
 
-@count_decorator
+@track_get_page
 def get_page(url: str) -> str:
+    """ Makes a http request to a given endpoint
     """
-    retrives the content of an html page, and also caches it
-    """
-    html = get(url)
-    return html.content
+    response = requests.get(url)
+    return response.text
